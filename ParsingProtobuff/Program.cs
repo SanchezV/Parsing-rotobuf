cc7f32ed57c8f06e10fcd30eac6da55946ab05ac
SanchezV <Vechkunin.ru@mail.ru>
2020-11-24 21:59:34 +0400

    Добавьте файлы проекта.

---
+++ ParsingProtobuff/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+namespace ParsingProtobuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] file = default;
+            if (File.Exists(@"c:\Users\Александр\Desktop\proto.chls"))
+            {
+                file = File.ReadAllBytes(@"c:\Users\Александр\Desktop\speedup_job_allow_tokens");
+            }
+            Stream stream = new MemoryStream(file);
+            var r = Google.Protobuf.CodedOutputStream.ComputeBytesSize(ByteString.CopyFrom(file));
+
+            var result = CodedInputStream.CreateWithLimits(stream, 65534, 65534);
+        }
+    }
+}
